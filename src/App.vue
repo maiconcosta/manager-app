3f0159e4ce7e7105962e07375ee9c60c0ae4050c
Maicon Costa <maiconcostawd@hotmail.com>
2021-06-05 12:56:28 -0300

    Instalação do vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,12 @@
 <template>
-  <a-locale-provider :locale="pt_BR">
-    <div id="app">   
-      <Main />
-    </div>
-  </a-locale-provider>
+  <div id="app">
+    <Main />  
+  </div>
 </template>
 
 <script>
-import pt_BR from 'ant-design-vue/lib/locale-provider/pt_BR'
-import Main from '@/layout/Main';
-
+import Main from '@/layout/Main'
 export default {
-  name: 'app',
-  data() {
-    return {
-      pt_BR,
-    }
-  },
   components: {
     Main
   }
--- src/layout/Main.vue
+++ src/layout/Main.vue
@@ -1,29 +1,33 @@
 <template>
-    <div class="main">    
-        <div class="side">
-            <Sidebar />            
-        </div>
-        <div class="content">
-           <h2>Router</h2>
-        </div>
-    </div>    
+  <div class="main">
+    <div class="side">
+      <Sidebar />
+    </div>
+    <div class="content">
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <script>
-import Sidebar from '@/layout/Sidebar';
+import Sidebar from "@/layout/Sidebar";
 export default {
-    name: 'Main',
-    components: {
-        Sidebar
-    }
-}
+  name: "Main",
+  components: {
+    Sidebar,
+  },
+};
 </script>
 
 <style scoped>
-    .main {
-        display: flex;
-    }
-    .content {
-        padding: 20px;
-    }  
+.main {
+  display: flex;
+}
+.content {
+  padding: 20px;
+}
 </style>
