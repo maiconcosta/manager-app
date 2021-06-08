a38aa0c607737014893d0c4eb290a994f502eb22
Maicon Costa <maiconcostawd@hotmail.com>
2021-06-08 10:18:22 -0300

    Criação do componente de card

--- src/views/Pedidos.vue
+++ src/views/Pedidos.vue
@@ -1,11 +1,34 @@
 <template>
   <div class="home">
     <h2>Pedidos</h2>
+    <a-row :gutter="16">
+      <a-col :span="12">
+        <Card
+          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+          commodi accusantium repellendus laudantium itaque, odio veritatis
+          ducimus ipsum, numquam quis quos porro dolorem nostrum magni delectus
+          dolor blanditiis. Esse, eligendi.</Card
+        >
+      </a-col>
+      <a-col :span="12">
+        <Card
+          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+          commodi accusantium repellendus laudantium itaque, odio veritatis
+          ducimus ipsum, numquam quis quos porro dolorem nostrum magni delectus
+          dolor blanditiis. Esse, eligendi.</Card
+        ></a-col
+      >
+    </a-row>
   </div>
 </template>
 
 <script>
+import Card from "@/components/Card.vue";
+
 export default {
   name: "Pedidos",
+  components: {
+    Card,
+  },
 };
 </script>
